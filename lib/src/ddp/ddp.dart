ee3c96c38624a37109d2715066ac112b0bdc08a5
纪元 <joyjiyuan@gmail.com>
2022-02-26 13:47:03 +0800

    Update ddp.dart

--- lib/src/ddp/ddp.dart
+++ lib/src/ddp/ddp.dart
@@ -314,9 +314,9 @@ class DDP implements ConnectionNotifier, StatusNotifier {
       this._version = '1';
       this._sessionId = msg['session'] as String;
       this._connectionListener!.forEach((l) => l());
+      this._ping();
     };
     this._messageHandlers!['ping'] = (msg) {
-      print('我收到了ping');
       this._pong();
     };
     this._messageHandlers!['pong'] = (msg) {
